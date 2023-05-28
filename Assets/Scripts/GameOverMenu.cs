81b53654066551ccf4a014a6ebe1b423ebeb5310
Aleyna Türkoğlu <aleynaturkoglu22@gmail.com>
2023-05-28 17:24:34 +0300

    soooon

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
     public void TekrarOyna()
     {
         // GameOver sahnesini yeniden yükle
